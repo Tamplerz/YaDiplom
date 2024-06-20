8ba3623dfe2f486da39a37f0db2e1b7aca6d7ca2
AndreyK <vlomak@yandex.ru>
2024-06-20 13:08:56 +0300

    version2-doravotka

--- create_order_test_v2.py
+++ create_order_test_v2.py
@@ -1,8 +1,11 @@
 # Андрей Кузнецов, 17-я когорта — Финальный проект. Инженер по тестированию плюс
 
 import sender_stand_request
+import data
 
 # Тест. Успешное создание заказа и проверка его записи в БД
 def test_getorder():
-    order_response = sender_stand_request.status_code_order
-    assert order_response == 200
+    response = sender_stand_request.post_new_order(data.order_body);
+    trackid = response.json()['track']
+    order_response = sender_stand_request.get_order(trackid, data.order_body2)
+    assert order_response.status_code == 200
