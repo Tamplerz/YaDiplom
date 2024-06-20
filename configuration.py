c9d60d9373399c652b044e07de8a80bcb6a64530
AndreyK <vlomak@yandex.ru>
2024-06-20 16:00:34 +0300

    version2-dorabotka2

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://7d3dbc83-2c45-48f7-aab1-144315257d20.serverhub.praktikum-services.ru/"
+URL_SERVICE = "https://aad18a51-6e50-4e15-b06a-3d9a33d33384.serverhub.praktikum-services.ru/"
 CREATE_ORDER_PATH = "/api/v1/orders"
 GET_ORDER_PATH = "/api/v1/orders/track?t="
--- create_order_test_v2.py
+++ create_order_test_v2.py
@@ -5,7 +5,7 @@ import data
 
 # Тест. Успешное создание заказа и проверка его записи в БД
 def test_getorder():
-    response = sender_stand_request.post_new_order(data.order_body);
+    response = sender_stand_request.post_new_order(data.order_body)
     trackid = response.json()['track']
     order_response = sender_stand_request.get_order(trackid, data.order_body2)
     assert order_response.status_code == 200
